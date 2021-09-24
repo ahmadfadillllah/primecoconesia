bb2ec64f2e09058451e25186147493b029a70600
ahmadfadillllah <ahmadfadillah502@gmail.com>
2021-09-24 17:56:55 +0800

    Update About

---
+++ resources/views/about.blade.php
@@ -0,0 +1,272 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>About - Primecoconesia</title>
+
+    <link rel="shortcut icon" href="favicon.png">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-114x114.png">
+
+    <link href="{{ asset('home') }}/fonts.googleapis.com/css069f.css?family=Open+Sans:400,400i,700,700i"
+        rel="stylesheet">
+    <link href="{{ asset('home') }}/fonts.googleapis.com/cssb119.css?family=Montserrat:400,500,700" rel="stylesheet">
+    <link href="{{ asset('home') }}/fonts.googleapis.com/css3f99.css?family=Playfair+Display:400,400i,700,700i"
+        rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('home/css/style.css') }}">
+</head>
+
+<body>
+
+    <div class="loader">
+        <div class="spinner">
+            <div class="double-bounce1"></div>
+            <div class="double-bounce2"></div>
+        </div>
+    </div>
+
+    <header class="navbar">
+        <div class="container">
+            <a href="#" class="brand js-target-scroll">
+                Primecoconesia
+            </a>
+
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <nav class="nav-desctop hidden-xs hidden-sm">
+                <ul class="nav-desctop-list">
+                    <li class="active"><a href="{{ route('home') }}">Home</a></li>
+                    <li><a href="{{ route('about') }}">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+
+            <nav class="nav-mobile hidden-md hidden-lg">
+                <div class="collapse navbar-collapse" id="nav-collapse">
+                    <ul class="nav-mobile-list">
+                        <li class="active"><a href="{{ route('home') }}">Home</a></li>
+                        <li><a href="{{ route('about') }}">About</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <main class="main main-inner main-about jarallax" data-jarallax='{"speed": 0.7}'>
+        <div class="container">
+            <div class="opener">
+                <h1>About Us.</h1>
+            </div>
+        </div>
+    </main>
+    <div class="content">
+        <section class="section bg-light">
+            <div class="container">
+                <div class="flex-md-middle row-space row-base row">
+                    <div class="col-space col-base col-md-12">
+                        <h3 class="section-title">Tentang kami</h3>
+                        <p>PT. Primecoconesia adalah perusahaan yang bergerak di bidang pengolahan kelapa yang fokus pada produksi minyak kelapa berkualitas tinggi. Tidak hanya menjadi produsen minyak kelapa berkualitas tinggi PT. Primecoconesia juga menjadi produsen berbagai produk turunan kelapa berkualitas tinggi, seperti: Kapuk, Briket, Cocopeat, Cocofiber dll.</p>
+                        <p>Berawal dari usaha rumahan di daerah sulawesi selatan yang mengolah kelapa dan turunannya menjadi produk bernilai, akhirnya dari bekal pengalaman yang dimiliki kami mulai masuk ke skala yang lebih besar.</p>
+                        <p>Akhirnya kami mempelajari potensi sumber daya pertanian khususnya kelapa di wilayah Sulawesi dan Jawa. Kemudian menemukan potensi produksi kelapa terbaik di berbagai daerah, dari temuan tersebut PT. Primecoconesia mulai melakukan pengolahan kelapa dalam skala besar menjadi minyak kelapa berkualitas tinggi dan memanfaatkan berbagai jenis turunan kelapa menjadi produk bernilai tinggi, seperti: Kopra, Sabut, Kapuk, Briket, Cocopeat, Cocofiber dll.</p>
+                        <p>Kami membangun kerjasama dan memberdayakan petani di berbagai wilayah di Indonesia (Sulawesi Selatan, Sulawesi Utara, Sulawesi Barat, Sulawesi Tenggara, Jawa Timur, Jawa Tengah dan Jawa Barat) untuk menghasilkan kelapa berkualitas tinggi dengan harga terbaik, dari kelapa berkualitas tinggi dihasilkan produk bernilai tinggi, seperti: minyak kelapa goreng,minyak kelapa murni, kopra, briket/arang, kapuk, cocopeat, cocofiber dan berbagai produk bernilai lainnya.</p>
+                        <p>Kami memproduksi kelapa dan turunannya melalui proses kerja terbaik agar mutu dan kualitas produk yang dihasilkan menjadi produk berkualitas tinggi. Proses kerja tersebut dilaksanakan secara bertahap, mulai dari proses penanaman, pembibitan, pemeriksaan, pengelohan hingga tercipta produk berkualitas tinggi dengan harga terbaik.</p>
+                        <p>Kami memiliki lahan perkebunan kelapa seluas 10.271 Hektar (Ha) dengan total produksi 20.356 ton/pertahun atau 5.089 ton/triwulan yang tersebar di berbagai wilayah di Indonesia (Sulsel-Sulbar-Sulteng-Sultra-Jatim-Jabar dan Jateng) yang di panen langsung oleh petani setempat dengan kualitas terbaik.</p>
+                        <p>Setelah merasakan keutungan dan melihat peluang yang menjanjikan serta permintaan yang terus meningkat dari waktu ke waktu, didukung dengan sumber daya kelapa yang melimpah di Indonesia maka PT. Primecoconesia berkomitmen untuk melakukan pengembangan dan perluasan dalam bidang produksi minyak kelapa berkualitas tinggi dan berbagai produk turunan kelapa lainnya.</p>
+                        <p>Produk turunan kelapa yang kami produksi meliputi kopra, batok kelapa, daun kelapa, sabut kelapa. Kami bekerjasama dengan investor untuk mengolah limbah turunan kelapa menjadi produk yang memiliki nilai produksi dan nilai jual, seperti cocofiber, cocopeat, arang kelapa/briket dan sebagainya.</p>
+                        <p>Saat ini kami sedang melakukan ekspansi pasar, memperluas komuditas dan menarik investor untuk memperbesar tempat pengolahan dan meningkatkan kuantitas serta kualitas produk. Kami juga menarik investor asing untuk melakukan ekspor ke kawasan eropa, asia pasifik, timur tengah, amerika dan sebagainya.</p>
+                    </div>
+                    {{-- <div class="col-space col-base col-md-6">
+                        <img class="img-responsive w100" src="img/7-540x670.jpg">
+                    </div> --}}
+                </div>
+            </div>
+        </section>
+
+        <section class="clients section pb-sm">
+            <div class="container">
+                <div class="col-md-10 col-lg-6 col-md-offset-1 col-lg-offset-3">
+                    <div class="client-carousel-wrp">
+                        <div class="client-carousel js-client-carousel">
+                            <div class="client">
+                                <h3>Visi</h3>
+                                <span class="quote">"</span>
+                                <p class="client-text">
+                                    Menjadi produsen terbaik dalam industri minyak kelapa dan turunannya yang diakui dan dipercaya secara nasional maupun internasional.</p>
+                            </div>
+                            <div class="client">
+                                <h3>Misi</h3>
+                                <span class="quote">"</span>
+                                <p class="client-text">
+                                    Menghasilkan minyak kelapa berkualitas tinggi</p>
+                                <p class="client-text">
+                                    Mengolah turunan kelapa menjadi produk bernilai tinggi</p>
+                                <p class="client-text">
+                                    Menghasilkan produk kelapa berkualitas tinggi dengan harga terbaik</p>
+                                <p class="client-text">
+                                    Melakukan kerja sama dengan perusahaan nasional dan internasional dalam ekspor produk</p>
+                                <p class="client-text">
+                                    Meningkatkan kuantitas dan kualitas produk agar dapat menjadi produk terbaik dunia</p>
+                                <p class="client-text">
+                                    Menjaga kepercayaan mitra dan pelanggan dengan terus menghasilkan produk kelapa berkualitas tinggi.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer id="footer" class="footer bg-dark">
+            <div class="container">
+                <div class="row-base row-space row">
+                    <div class="brand-info col-base col-space col-sm-6 col-md-3">
+                        <a href="#top" class="brand js-target-scroll">
+                            Primecoconesia.
+                        </a>
+                        <p>Terima kasih atas minat Anda pada produk kami.</p>
+                        <address><strong class="text-white">E-mail:</strong> <a
+                                href="http://paul-themes.com/cdn-cgi/l/email-protection" class="__cf_email__"
+                                data-cfemail="503528313d203c35103528313d203c357e2225">[email&#160;protected]</a>
+                        </address>
+                    </div>
+                    {{-- <div class="col-base col-space col-sm-6 col-md-2">
+                        <strong class="footer-title">Links</strong>
+                        <ul class="nav-bottom">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Product</a></li>
+                            <li><a href="#">Portfolio</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Contacts</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-base col-space col-sm-6 col-md-4">
+                        <strong class="footer-title">Links</strong>
+                        <ul class="nav-bottom">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Product</a></li>
+                            <li><a href="#">Portfolio</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Contacts</a></li>
+                        </ul>
+                    </div> --}}
+                    {{-- <div class="col-base col-space col-sm-6 col-md-6">
+                        <strong class="footer-title">Newsletter</strong>
+                        <form class="subscribe-form js-subscribe-form">
+                            <div class="input-group">
+                                <input id="mc-email" type="email" class="input-round form-control" placeholder="Email">
+                                <span class="input-group-btn">
+                                    <button class="btn" type="submit"><span class="icon-arrow-right"></span></button>
+                                </span>
+                            </div>
+                            <label class="mc-label" for="mc-email" id="mc-notification"></label>
+                        </form>
+                    </div> --}}
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <div class="container">
+                    <div class="copyrights">
+                        © Copyright 2021 Primecoconesia. All Rights Reserved
+                    </div>
+                    <ul class="social-list">
+                        <li><a href="#" class="fa fa-facebook"></a></li>
+                        <li><a href="#" class="fa fa-twitter"></a></li>
+                        <li><a href="#" class="fa fa-linkedin"></a></li>
+                        <li><a href="#" class="fa fa-instagram"></a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    </div>
+
+    <div id="request" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <span class="close" data-dismiss="modal" aria-label="Close">&times;</span>
+                    <h2 class="modal-title">Send request</h2>
+                </div>
+                <div class="modal-body">
+                    <form class="form-request js-ajax-form">
+                        <div class="form-group">
+                            <input type="text" name="name" placeholder="Name">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" name="email" required placeholder="Email *">
+                        </div>
+                        <div class="form-group">
+                            <textarea rows="3" name="message" placeholder="Message"></textarea>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-block">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="success" class="modal modal-message fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <span class="close" data-dismiss="modal" aria-label="Close"><span
+                            aria-hidden="true">&times;</span></span>
+                    <h2 class="modal-title">Thank you</h2>
+                    <p class="modal-subtitle">Your message is successfully sent...</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="error" class="modal modal-message fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <span class="close" data-dismiss="modal" aria-label="Close"><span
+                            aria-hidden="true">&times;</span></span>
+                    <h2 class="modal-title">Sorry</h2>
+                    <p class="modal-subtitle"> Something went wrong </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script data-cfasync="false" src="{{ asset('home') }}/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+    <script src="{{ asset('home') }}/js/jquery.min.js"></script>
+    <script src="{{ asset('home') }}/js/bootstrap.min.js"></script>
+    <script src="{{ asset('home') }}/js/smoothscroll.js"></script>
+    <script src="{{ asset('home') }}/js/jquery.viewport.js"></script>
+    <script src="{{ asset('home') }}/js/jquery.validate.min.js"></script>
+    <script src="{{ asset('home') }}/js/wow.min.js"></script>
+    <script src="{{ asset('home') }}/js/jquery.ajaxchimp.min.js"></script>
+    <script src="{{ asset('home') }}/js/jarallax.js"></script>
+    <script src="{{ asset('home') }}/js/jquery.magnific-popup.js"></script>
+    <script src="{{ asset('home') }}/js/owl.carousel.min.js"></script>
+    <script src="{{ asset('home') }}/js/isotope.pkgd.min.js"></script>
+    <script src="{{ asset('home') }}/js/imagesloaded.pkgd.js"></script>
+
+    <script src="{{ asset('home') }}/js/rev-slider/jquery.themepunch.tools.min.js"></script>
+    <script src="{{ asset('home') }}/js/rev-slider/jquery.themepunch.revolution.min.js"></script>
+
+    <script src="{{ asset('home') }}/js/rev-slider/revolution.extension.actions.min.js"></script>
+    <script src="{{ asset('home') }}/js/rev-slider/revolution.extension.carousel.min.js"></script>
+    <script src="{{ asset('home') }}/js/rev-slider/revolution.extension.kenburn.min.js"></script>
+    <script src="{{ asset('home') }}/js/rev-slider/revolution.extension.layeranimation.min.js"></script>
+    <script src="{{ asset('home') }}/js/rev-slider/revolution.extension.migration.min.js"></script>
+    <script src="{{ asset('home') }}/js/rev-slider/revolution.extension.navigation.min.js"></script>
+    <script src="{{ asset('home') }}/js/rev-slider/revolution.extension.parallax.min.js"></script>
+    <script src="{{ asset('home') }}/js/rev-slider/revolution.extension.slideanims.min.js"></script>
+    <script src="{{ asset('home') }}/js/rev-slider/revolution.extension.video.min.js"></script>
+    <script src="{{ asset('home') }}/js/rev-slider-init.js"></script>
+    <script src="{{ asset('home') }}/js/interface.js"></script>
+</body>
+
+</html>
